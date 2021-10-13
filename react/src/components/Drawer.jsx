5bddbb2dfd6dfef05cdb7041c937114ee43710ba
mcken-vince <mcken.vince@gmail.com>
2021-10-13 13:15:21 -0600

    Remove unnecessary imports

--- react/src/components/Drawer.jsx
+++ react/src/components/Drawer.jsx
@@ -2,11 +2,7 @@ import { useState, Fragment } from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+
 
 export default function NewDrawer(props) {
   // pass in anchor as props 'left', 'right', 'top', 'bottom'
